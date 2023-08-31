a2cf612f78b6112b4aa2a3b1e0e85df7394ef0da
Fn4Us <114163250+Fn4Us@users.noreply.github.com>
2023-08-30 17:14:09 -0700

    IT WORKS

--- u-bot9000.py
+++ u-bot9000.py
@@ -25,7 +25,7 @@ randomReply = [
       "  UUUUU    ###    H        H\n \n"
       "  U        U      #       H        H\n \n"
       "  U        U                  HHH"
-]
+] #Reddit ascii is so finicky
 
 keep_alive()
 
@@ -34,7 +34,7 @@ def run():
       for comment in reddit.redditor("h-bot-model-h").stream.comments(skip_existing=True):
          random_response = random.choice(randomReply)
          print("new comment")
-         comment.reply(random_reply)
+         comment.reply(random_response)
          print(f"replied with: {random_response}")
     except Exception as e:
       print(e)
@@ -43,16 +43,16 @@ run()
 def process_new_posts(subreddit_name, reply_text):
     subreddit = reddit.subreddit(subreddit_name)
     for submission in subreddit.stream.submissions(skip_existing=True):
-        if submission.author and submission.author.name != username_to_check:
+        if submission.author and submission.author.name != username_to_check: #This code is useless it already skips_existing
             submission.reply(reply_text)
             print(f"Replied to the post with ID: {submission.id}")
             print(f"Post Title: {submission.title}")
 
-# Create two threads for concurrent execution of the functions
+# Create two threads for both of the functions
 thread_run = threading.Thread(target=run)
 thread_process = threading.Thread(target=process_new_posts, args=('theletterh', 'H is cool, and so is U \n\n ^(I am a bot, and this action was performed automatically. If you think this is a mistake, leave TheLetterH. If you still think this is a mistake, please) [^(contact me here.)](https://www.reddit.com/message/compose/?to=u-bot9000)'))
 
-# Start both threads
+# Start both
 thread_run.start()
 thread_process.start()
 
