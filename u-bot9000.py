e806f7f9e996380f30855d8a95254e958b897e7e
Fn4Us <114163250+Fn4Us@users.noreply.github.com>
2024-01-21 17:57:48 -0800

    Update u-bot9000.py

--- u-bot9000.py
+++ u-bot9000.py
@@ -1,84 +1,84 @@
 import praw
 import random
 import threading
-from keep_alive import keep_alive
 
-reddit = praw.Reddit(
-    client_id="W1T_uV33O1NvXwmtVbwH-Q",
-    client_secret="#####################",
-    user_agent="<console:u-bot9000:1.0>",
-    username="u-bot9000",
-    password="###########")
 
-# List of phrases and their corresponding responses
-phrases_and_responses = {
-    "!a_useless_name": "Its a joke \n \n It seems you forgot the I in ai",
-    "!anarchy": "Qooqle en passant \n \nHoly Hell!\n \nNew response just dropped\n \nActual Zombie!\n \n Call the exorcist!\n \nPawn storm incoming!\n \nQueen sacrifice, anyone?",
-    "Good bot": "Thanks! Good human",
-    "!github": "https://github.com/Fn4Us/u-bot9000"
-}
+reddit = praw.Reddit(client_id="XXXXXXXXXXXXXXXXXXXXX",
+                     client_secret="XXXXXXXXXXXXXXXXXXXXX",
+                     user_agent="<console:u-bot9000:1.0>",
+                     username="u-bot9000",
+                     password="XXXXXXXXXXXXXXXXXXXXXX")
 
-subreddit_data = [
-    {'name': 'theletterh', 'reply_text': 'H is cool, and so is U \n\n ^(I am a bot, and this action was performed automatically. If you think this is a mistake, leave TheLetterH. If you still think this is a mistake, please) [^(contact me here.)](https://www.reddit.com/message/compose/?to=u-bot9000&subject=The%20bot%27s%20broken)'},
-    {'name': 'theletteru', 'reply_text': 'egg'} # Ignore this one the code fails when I remove it
-]
 
-username_to_check = 'u-bot9000' # This is blunderful code but I dont want to change it
+print('running')
+
+subreddit = reddit.subreddit("theletterh")
+
+username_to_check = 'NotAlreadyUsed'  # This is blunderful code but I dont want to change it
 
 randomReply = [
-    "H and U are both good. HUnion!!!",
-    "I love H, I also love U",
-    "HU 👍",
-    "H + U = 👍",
-    "HEIJAK needs U",
-    "H ❤️ U",
-    "𝙃𝙐",
-    "Move aside 𝕏, we have ℍ𝕌",
-    "Ƕ"
+    "H and U are both good. HUnion!!!", "I love H, I also love U", "HU 👍",
+    "H + U = 👍", "HEIJAK needs U", "H ❤️ U", "𝙃𝙐", "Move aside 𝕏, we have ℍ𝕌",
+    "Ƕ", "}·{ ]_[", "HU her? I hardly know her!", "H and U are the best, AMA", "I am ***H***appy this is working, are yo***U***?"
 ]
 
-keep_alive()
+replies = ["h", "ℋ", "*h*", "**h**", "~~literally anything else~~ h", "h is awesome :]", "h :]", "# h", "# **h**"]
+
 
 def run():
-    try:
-        for comment in reddit.redditor("h-bot-model-h").stream.comments(skip_existing=True):
-            random_response = random.choice(randomReply)
-            print("new comment")
-            comment.reply(random_response)
-            print(f"replied with: {random_response}")
-    except Exception as e:
-        print(e)
-        run()
-
-def process_new_posts(subreddit_name, reply_text):
-    subreddit = reddit.subreddit(subreddit_name)
-    for submission in subreddit.stream.submissions(skip_existing=True):
-        if submission.author and submission.author.name != username_to_check:
-            prob = random.randint(0,1000)
-            if prob == 1:
-              probe = random.randint(0,10)
-              if probe == 1:
-                print("MEGA JACKPOT!!!")
-                comment.reply("LUCKY U!!!! THIS COMMENT HAS A 1/10000 CHANCE OF HAPPENINQ!!!")
-              else:
-                print("JACKPOT")
-                comment.reply("Lucky H! This comment has a 1/1000 chance of happeninq!")
-            else:
-              submission.reply(reply_text)
-            print(f"Replied to the post with ID: {submission.id}")
-            print(f"Post Title: {submission.title}")
+  try:
+    for comment in subreddit.stream.comments(skip_existing=True):
+      if comment.author and comment.author.name == "h-bot-model-h" and comment.body in replies:
+        random_response = random.choice(randomReply)
+        print("new comment")
+        comment.reply(random_response)
+        print(f"replied with: {random_response}")
+  except Exception as e:
+    print(e)
+    run()
 
 
-# Create two threads for concurrent execution of the functions
+def process_new_posts(subreddit_name):
+  subreddit = reddit.subreddit(subreddit_name)
+  for submission in subreddit.stream.submissions(skip_existing=True):
+    if submission.author and submission.author.name != username_to_check:
+      prob = random.randint(0, 1000)
+      if prob == 1:
+        probe = random.randint(0, 10000)
+        if probe == 1:
+          print("MEGA JACKPOT!!!")
+          submission.reply(
+              "LUCKY U!!!! THIS COMMENT HAS A 1/10000 CHANCE OF HAPPENINQ!!!")
+        else:
+          print("JACKPOT")
+          submission.reply(
+              "Lucky H! This comment has a 1/1000 chance of happeninq!")
+      else:
+        prob = random.randint(0, 2)
+        if prob == 1:
+          submission.reply(
+              "H is wonderful, and so is U \n\n ^(I am a bot, and this action was performed automatically. If you think this is a mistake, leave TheLetterH. If you still think this is a mistake, please) [^(contact me here.)](https://www.reddit.com/message/compose/?to=u-bot9000&subject=The%20bot%27s%20broken)"
+          )
+        else:
+          submission.reply(
+              "H is cool, and so is U \n\n ^(I am a bot, and this action was performed automatically. If you think this is a mistake, leave TheLetterH. If you still think this is a mistake, please) [^(contact me here.)](https://www.reddit.com/message/compose/?to=u-bot9000&subject=The%20bot%27s%20broken)"
+          )
+      print(f"Replied to the post with ID: {submission.id}")
+      print(f"Post Title: {submission.title}")
+
+
+## Create two threads for concurrent execution of the functions
 thread_run = threading.Thread(target=run)
-thread_process = threading.Thread(target=process_new_posts, args=('theletterh', 'H is cool, and so is U \n\n ^(I am a bot, and this action was performed automatically. If you think this is a mistake, leave TheLetterH. If you still think this is a mistake, please) [^(contact me here.)](https://www.reddit.com/message/compose/?to=u-bot9000&subject=The%20bot%27s%20broken)'))
-
+thread_process = threading.Thread(
+    target=process_new_posts,
+    args=('theletterh', ))  # Pass subreddit_name as a tuple
 
 # Start all threads
 thread_run.start()
 thread_process.start()
 
-
 # Wait for all threads to finish
+thread_run.join()
+thread_process.join()
 thread_run.join()
 thread_process.join()
